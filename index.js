6c7e518e4c1e395d26f010d5a4026eeb15161fb0
michoel <michoelr111@gmail.com>
2023-05-19 16:07:32 -0400

    complete

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      const element = array[j];
+      if (array[i] + array[j] === target) {
+        return true
+      }
+    }
+  } return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n²)
 */
 
 /* 
   Add your pseudocode here
+  I will need to go through the entire array and check if any 2 numbers
+  equal the target
 */
 
 /*
   Add written explanation of your solution here
+  I wrote one loop which logs i and a nested loop which logs j (which is i + 1)
+  if both array[j] ever === array[i] then i return true otherwise i return false
 */
 
 // You can run `node index.js` to view these console logs
